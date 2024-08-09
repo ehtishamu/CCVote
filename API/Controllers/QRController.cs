7aaf09a76fd66043c91c2975e9b59d6a741797e7
Ehtisham Ulhaq <92743163+ehtishamu@users.noreply.github.com>
2024-08-09 15:26:18 -0700

    api integration

---
+++ API/Controllers/QRController.cs
@@ -0,0 +1,42 @@
+﻿using Aspose.BarCode.Generation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QRController : ControllerBase
+    {
+        
+        [HttpPost]
+        public IActionResult Post(Dictionary<string, string>[] KeyValuePairs)
+        {
+            for (int i = 0; i < KeyValuePairs.Length; i++)
+            {
+                var generator = new BarcodeGenerator(EncodeTypes.QR);
+                // Specify code text to encode
+               
+                    KeyValuePairs.ToList()[i].TryGetValue("URL",out string colValue);
+                generator.CodeText = colValue;
+                // Specify the size of the image
+                generator.Parameters.Barcode.XDimension.Pixels = 8;
+                generator.Parameters.Resolution = 500;
+                // Save the generated QR code
+                generator.Save("C:\\Ehtisham\\CCLIVE\\CCVote\\CCVote\\wwwroot\\img"+i+".jpg");
+            }
+            return Ok(new
+            {
+                message = "QR Generated Successfully!!!",
+            });
+        }
+        private string TryGetValueFromDist(Dictionary<string, string> rowData, string key)
+        {
+            rowData.TryGetValue(key, out string colValue);
+            return colValue;
+        }
+    }
+    public class QRModel { 
+    
+    }
+}
--- CCVote/Program.cs
+++ CCVote/Program.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<PdfService>();
+
+// Add services to the container.
 
 
 await builder.Build().RunAsync();
